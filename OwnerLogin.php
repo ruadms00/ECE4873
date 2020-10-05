50c5794fe51b85ac56cd27f8ce21c4ad5253da1e
Josh Smith <pentexjosh@gmail.com>
2020-10-04 22:16:34 -0400

    HTML and PHP files uploaded

---
+++ OwnerLogin.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Boombox</title>
+    
+    <link href='https://fonts.googleapis.com/css?family=Strait' rel='stylesheet'>
+    <style type="text/css">
+    <!--
+    body {
+      color:#0080FF;
+      font-family: 'Strait';font-size: 22px;
+      background-color:#4B4B4B;
+      background-image:url('Background Image');
+      background-repeat:no-repeat;
+    }
+    a  { color:#0000FF; }
+    a:visited { color:#800080; }
+    a:hover { color:#008000; }
+    a:active { color:#FF0000; }
+
+	h1 {
+	   text-align: center;
+	}
+	
+  input[type=text], input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+  }
+  
+  button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+  }
+  
+  button:hover {
+    opacity: 0.8;
+  }
+  
+  .cancelbtn {
+    width: auto;
+    padding: 10px 20px;
+    background-color: #f44336;
+	float:right;
+  }
+  
+  .left {
+    padding: 16px;
+	width:50%;
+	float:left;
+  }
+  
+  .right {
+    padding: 16px;
+	margin-left:50%;
+  }
+  
+  span.psw {
+    float: right;
+    padding-top: 16px;
+  }
+  
+  /* Change styles for span and cancel button on extra small screens */
+  @media screen and (max-width: 300px) {
+    span.psw {
+       display: block;
+       float: none;
+    }
+    .cancelbtn {
+       width: 100%;
+    }
+  }
+    -->
+    </style>
+    <!--[if IE]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <header>
+  <h1>
+  BOOMBOX OWNER
+    </h1>
+  </header>
+  
+<form action="/action_page.php" method="post">
+
+  <div class="left">
+    <label for="dname"><b>Device</b></label>
+    <input type="text" placeholder="Enter Device ID" name="devid" required>
+    
+    <label for="pw"><b>Password</b></label>
+    <input type="text" placeholder="Enter Password" name="pw" required>
+
+    <button type="submit">Connect</button>
+    <label>
+      <input type="checkbox" checked="checked" name="rem"> Connect Automatically
+    </label>
+    <a href="index.html"><button type="button" class="cancelbtn">Return</button></a>
+  </div>
+  <div class="right">
+ <label>
+ OR SCAN QR CODE HERE
+ </label>
+  </div>
+</form>
+
+  </body>
+</html>
