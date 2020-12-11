03dde6858cfb154c6d41702f21690dc39715c01f
Josh Smith <kjang41@gatech.edu>
2020-12-11 14:13:19 -0500

    modified:   SpotifyAction.py

--- SpotifyAction.py
+++ SpotifyAction.py
@@ -16,18 +16,83 @@ class Spotify:
         self.getPlaylist()
         self.checkDevice()
         self.play_start()
-
-    def checkDevice(self):
-        query = "https://api.spotify.com/v1/me/player/devices"
-        response = requests.get(
-            query,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
+    
+    
+    def getReq(self,req):
+        query = "https://api.spotify.com/" + req
         try:
-            devices = response.json()["devices"]
+            response = requests.get(
+                query,
+                headers={
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer {}".format(self.spotify_token)
+                }
+            )
+            variables = vars(response)
+            if(variables['status_code'] == 200):
+                return response.json()
+            elif(variables['status_code'] == 204):
+                print(query)
+                print('status fail')
+                return False
+        except:
+            print(query)
+            print('exception reached')
+            return False
+        
+    def putReq(self, req, body = None):
+        query = "https://api.spotify.com/" + req
+        try:
+            response = requests.put(
+                query,
+                data=body,
+                headers={
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer {}".format(self.spotify_token)
+                }  
+            )
+        except:
+            print(query)
+            print('exception reached')
+            return False
+    
+    def delReq(self, req, body = None):
+        query = "https://api.spotify.com/" + req
+        try:
+            response = requests.delete(
+                query,
+                data=body,
+                headers={
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer {}".format(self.spotify_token)
+                }  
+            )
+        except:
+            print(query)
+            print(body)
+            print('exception reached')
+            return False
+        
+    def postReq(self, req, body = None):
+        query = "https://api.spotify.com/" + req
+        try:
+            response = requests.post(
+                query,
+                data=body,
+                headers={
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer {}".format(self.spotify_token)
+                }  
+            )
+        except:
+            print(query)
+            print('exception reached')
+            return False
+        
+    def checkDevice(self):
+        response = self.getReq("v1/me/player/devices")
+        if response:
+            devices = response["devices"]
             device = 0
             for i in devices:
                 if i["name"][0:10] == "raspotify ":
@@ -37,361 +102,173 @@ class Spotify:
                         self.offset = 0
                         self.play_start()
                     break
-        except:
-            return False
 
     def getSongInfo(self):
-        query = "https://api.spotify.com/v1/me/player/currently-playing"
-        response = requests.get(
-            query,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
-        try:
-            variables = vars(response)
-            if(variables['status_code'] == 200):
-                return response.json()
-            elif(variables['status_code'] == 204):
-                return False
-        except:
-            return False
+        response = self.getReq("v1/me/player/currently-playing")
+        return response
 
     def getArt(self):
-        query = "https://api.spotify.com/v1/me/player/currently-playing"
-        response = requests.get(
-            query,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
-        try:
-            return response.json()['item']['album']['images'][1]['url']
-        except:
+        response = self.getReq("v1/me/player/currently-playing")
+        if response:
+            return response['item']['album']['images'][1]['url']
+        else:
             return False
         
     def isPlaying(self):
-        query = "https://api.spotify.com/v1/me/player/currently-playing"
-        response = requests.get(
-            query,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
-        try:
-            return response.json()['is_playing']
-        except:
+        response = self.getReq("v1/me/player/currently-playing")
+        if response:
+            return response['is_playing']
+        else:
             return False
         
     def getName(self):
-        query = "https://api.spotify.com/v1/me/player/currently-playing"
-        response = requests.get(
-            query,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
-        try:
-            return response.json()['item']['name']
-        except:
+        response = self.getReq("v1/me/player/currently-playing")
+        if response:
+            return response['item']['name']
+        else:
             return False
         
     def getSongUri(self):
-        query = "https://api.spotify.com/v1/me/player/currently-playing"
-        response = requests.get(
-            query,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
-        try:
-            return response.json()['item']['uri']
-        except:
+        response = self.getReq("v1/me/player/currently-playing")
+        if response:
+            return response['item']['uri']
+        else:
             return False
         
     def getArtist(self):
-        query = "https://api.spotify.com/v1/me/player/currently-playing"
-        response = requests.get(
-            query,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
-        try:
-            return response.json()['item']['artists'][0]['name']
-        except:
+        response = self.getReq("v1/me/player/currently-playing")
+        if response:
+            return response['item']['artists'][0]['name']
+        else:
             return False
     
     def getLength(self):
-        query = "https://api.spotify.com/v1/me/player/currently-playing"
-        response = requests.get(
-            query,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
-        try:
-            return response.json()['item']['duration_ms'] // 1000
-        except:
+        response = self.getReq("v1/me/player/currently-playing")
+        if response:
+            return response['item']['duration_ms'] // 1000
+        else:
             return False
             
     def getQueue(self):
-        query = "https://api.spotify.com/v1/playlists/{}/tracks".format(
-            self.playlist_id)
-        response = requests.get(
-            query,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
-        response_json = 0
-        try:
-            response_json = response.json()["items"]
-        except:
+        response = self.getReq("v1/playlists/{}/tracks".format(self.playlist_id))
+        items = []
+        if response:
+            items = response["items"]
+        else:
             return []
         queue = []
-        for i in response_json:
+        for i in items:
             song = i["track"]["name"]
             artist = i["track"]["artists"][0]["name"]
             queue.append(song + "::" + artist)
         return queue
         
     def getPlaylist(self):
-        try:
-            query = "https://api.spotify.com/v1/users/{}/playlists".format(spotify_user_id)
-            response = requests.get(
-                query,
-                headers={
-                    "Content-Type": "application/json",
-                    "Authorization": "Bearer {}".format(self.spotify_token)
-                }
-            )
-            response = response.json()["items"]
+        response = self.getReq("v1/users/{}/playlists".format(spotify_user_id))
+        if response:
+            items = response["items"]
             exists = False
-            for v in response:
+            for v in items:
                 if v["name"] == "Boombox Playlist":
                     exists = True
-                    self.playlist_uri= v["uri"]
+                    self.playlist_uri = v["uri"]
                     self.playlist_id = v["uri"].split(':')[2]
-                if not exists:
-                    self.create_playlist()
-        except:
+            if not exists:
+                self.create_playlist()
+        else:
             self.create_playlist()
     
     def create_playlist(self):
         # Create a new playlist
-        query = "https://api.spotify.com/v1/users/{}/playlists".format(spotify_user_id)
-        
-        request_body = json.dumps({
-            "name": "Boombox Playlist", "description": "Songs from SMS", "public": True
-        })
-        response = requests.post(query, data=request_body, headers={
-            "Content-Type": "application/json",
-            "Authorization": "Bearer {}".format(self.spotify_token)
-        })
-        response_json = response.json()
-        try:
+        jsond = json.dumps({"name": "Boombox Playlist", "description": "Boombox Queue", "public": True})
+        response = self.postReq("v1/users/{}/playlists".format(spotify_user_id), jsond)
+        if response:
             self.playlist_uri = response_json["uri"]
             self.playlist_id = response_json["uri"].split(':')[2]
-        except:
+        else:
             return False
     
     def play_start(self):
-        query = "https://api.spotify.com/v1/me/player/play?device_id={}".format(self.deviceId)
-        request_body = json.dumps({
+        jsond = json.dumps({
             "context_uri":"{}".format(self.playlist_uri),
-            "offset":{"position":self.offset}})
-        response = requests.put(
-            query,
-            data=request_body,
-            headers={
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
-        
+            "offset":{"position":self.offset}
+        })
+        response = self.putReq("v1/me/player/play?device_id={}".format(self.deviceId),jsond)
+
     def play(self):
-        query = "https://api.spotify.com/v1/me/player/play"
-        response = requests.put(
-            query,
-            headers={
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
+        self.putReq("v1/me/player/play")
         
     def pause(self):
-        query = "https://api.spotify.com/v1/me/player/pause"
-        request_body = json.dumps({"context_uri": "{}".format(self.playlist_uri)})
-        response = requests.put(query, data=request_body,
-            headers={
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
+        self.putReq("v1/me/player/pause")
         
     def nextSong(self):
-        query = "https://api.spotify.com/v1/me/player/next"
-        response = requests.post(query,
-            headers={
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
+        self.postReq("v1/me/player/next")
         
     def prevSong(self):
-        query = "https://api.spotify.com/v1/me/player/previous"
-        response = requests.post(query,
-            headers={
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
+        self.postReq("v1/me/player/previous")
         
     def shuffle(self):
-        query = "https://api.spotify.com/v1/me/player"
-        response = requests.get(
-            query,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
-        response_json = response.json()
-        shuffle = response_json["shuffle_state"]
-        query = "https://api.spotify.com/v1/me/player/shuffle?state={}".format(
-            not shuffle)
-        response = requests.put(query,
-            headers={
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
-        
+        response = self.getReq("v1/me/player")
+        shuffle = response["shuffle_state"]
+        self.putReq("v1/me/player/shuffle?state={}".format(not shuffle))
+
     def repeat(self):
-        query = "https://api.spotify.com/v1/me/player"
-        response = requests.get(
-            query,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
-        response_json = response.json()
-        repeat = response_json["repeat_state"]
+        response = self.getReq("v1/me/player")
+        repeat = response["repeat_state"]
         if (repeat == "context"):
             repeat = "off"
         else:
             repeat = "context"
-        query = "https://api.spotify.com/v1/me/player/repeat?state={}".format(
-            repeat)
-        response = requests.put(query,
-            headers={
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
+            self.putReq("v1/me/player/repeat?state={}".format(repeat))
         
     def formatSong(self, txt):
         data = txt.split("::")
         song_name = data[0]
         artist = data[1]
-        query = "https://api.spotify.com/v1/search?query=track%3A{}+artist%3A{}&type=track&offset=0&limit=20".format(
+        response = self.getReq("v1/search?query=track%3A{}+artist%3A{}&type=track&offset=0&limit=20".format(
             song_name,
             artist
-        )
-        response = requests.get(
-            query,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }
-        )
-        try:
-            response_json = response.json()["tracks"]["items"]
-            if(len(response_json) == 0):
+        ))
+        if response:
+            items = response["tracks"]["items"]
+            if len(items) == 0:
                 return False
-            song = response_json[0]["uri"]
-            return song
-        except:
+            return items[0]["uri"]
+        else:
             return False
     
     def addQueue(self, song):
-        query = "https://api.spotify.com/v1/me/player/queue?uri={}".format(song)
-        response = requests.post(
-            query,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }
-        )
+        self.postReq("v1/me/player/queue?uri={}".format(song))
         
     def add(self, song):
         song = self.formatSong(song)
-        query = "https://api.spotify.com/v1/playlists/{}/tracks?uris={}".format(
-            self.playlist_id, song)
-        response = requests.post(
-            query,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }
-        )
+        self.postReq("v1/playlists/{}/tracks?uris={}".format(self.playlist_id, song)) 
         self.songAdded = True
     
 
     def remove(self, song):
         #this removes all of the same song, you could search for a specific song though
         song = self.formatSong(song)
-        query = "https://api.spotify.com/v1/playlists/{}/tracks".format(
-            self.playlist_id)
-        request_body = json.dumps(
+        jsond = json.dumps(
             { "tracks": [{"uri": "{}".format(song)}]})
-        response = requests.delete(
-            query,
-            data=request_body,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }
-        )
+        self.delReq("v1/playlists/{}/tracks".format(self.playlist_id), jsond)
         
     def clear(self):
-        query = "https://api.spotify.com/v1/playlists/{}/followers".format(
-            self.playlist_id)
-        response = requests.delete(
-            query,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }
-        )
+        self.delReq("v1/playlists/{}/followers".format(self.playlist_id))
         self.create_playlist()
 
     def updateLocation(self):
         song_uri = self.getSongUri()
         if song_uri == False:
-            self.offset = 0
-            return
-        query = "https://api.spotify.com/v1/playlists/{}/tracks".format(
-            self.playlist_id)
-        response = requests.get(
-            query,
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": "Bearer {}".format(self.spotify_token)
-            }  
-        )
-        response_json = False
-        try:
-            response_json = response.json()["items"]
-        except:
+            return False
+        response = self.getReq("v1/playlists/{}/tracks".format(self.playlist_id))
+        items = False
+        if response:
+            items = response["items"]
+        else:
             return False
         j = 0
-        for i in response_json:
+        for i in items:
             song = i["track"]["uri"]
             if song == song_uri:
                 if j == 0 and self.offset > 0:
